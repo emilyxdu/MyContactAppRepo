eb95845f014e88d52c43656a4a7dd1c294b1163b
Emily Du <emilyxdu@gmail.com>
2017-05-15 09:23:22 -0700

    Added insertData and addBoolean methods

---
+++ MyContactApp/app/src/main/java/com/example/emily/mycontactapp/DatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.example.emily.mycontactapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by emily on 5/15/2017.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+        public static final String DATABASE_NAME = "Contact.db";
+        public static final String TABLE_NAME = "Contact_table";
+        public static final String COL_1 = "ID";
+        public static final String COL_2 = "NAME";
+
+        public DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+
+
+        public boolean  insertData(String name){
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(COL_2, name);
+
+            long result = db.insert(TABLE_NAME, null, contentValues);
+            if(result == -1) return false;
+            else return true;
+        }
+}
--- MyContactApp/app/src/main/java/com/example/emily/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/emily/mycontactapp/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.emily.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    EditText editName;
+    Button btnAddData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_name);
     }
+
+
+    public void addBoolean (View v) {
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+        if(isInserted == true){
+            Log.d("MyContact", "Data insertion successful");
+            //create toast message to user indicating data inserted correctly
+
+        }
+        else{
+            Log.d("MyContact", "Data insertion unsuccessful");
+            //create toast message to user indicating data inserted incorrectly
+
+        }
+    }
+
+
+
 }
