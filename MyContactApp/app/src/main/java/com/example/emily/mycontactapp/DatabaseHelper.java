8d57986acdc15d3807b45061b8cd70aaba2efc43
Emily Du <emilyxdu@gmail.com>
2017-05-16 09:28:41 -0700

    Added viewData and getAllData, updated insertData

--- MyContactApp/app/src/main/java/com/example/emily/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/emily/mycontactapp/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.emily.mycontactapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -15,6 +16,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         public static final String TABLE_NAME = "Contact_table";
         public static final String COL_1 = "ID";
         public static final String COL_2 = "NAME";
+        public static final String COL_3 = "AGE";
+        public static final String COL_4 = "PHONE NUMBER";
 
         public DatabaseHelper(Context context) {
             super(context, DATABASE_NAME, null, 1);
@@ -33,13 +36,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
 
 
-        public boolean  insertData(String name){
+        public boolean  insertData(String name, String age, String phone_number){
             SQLiteDatabase db = this.getWritableDatabase();
             ContentValues contentValues = new ContentValues();
             contentValues.put(COL_2, name);
+            contentValues.put(COL_3, age);
+            contentValues.put(COL_4, phone_number);
 
             long result = db.insert(TABLE_NAME, null, contentValues);
             if(result == -1) return false;
             else return true;
+        }
+
+
+        public Cursor getAllData() {
+            SQLiteDatabase db = this.getWritableDatabase();
+            Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+            return res;
         }
 }
--- MyContactApp/app/src/main/java/com/example/emily/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/emily/mycontactapp/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.emily.mycontactapp;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editPhoneNumber;
     Button btnAddData;
 
 
@@ -22,21 +26,47 @@ public class MainActivity extends AppCompatActivity {
         myDb = new DatabaseHelper(this);
 
         editName = (EditText) findViewById(R.id.editText_name);
+        editAge = (EditText) findViewById(R.id.editText_age);
+        editPhoneNumber = (EditText) findViewById(R.id.editText_phonenumber);
     }
 
 
     public void addBoolean (View v) {
-        boolean isInserted = myDb.insertData(editName.getText().toString());
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editAge.getText().toString(), editPhoneNumber.getText().toString());
         if(isInserted == true){
             Log.d("MyContact", "Data insertion successful");
             //create toast message to user indicating data inserted correctly
+            Toast.makeText(getApplicationContext(), "Data insertion successful", Toast.LENGTH_SHORT).show();
+
 
         }
         else{
             Log.d("MyContact", "Data insertion unsuccessful");
             //create toast message to user indicating data inserted incorrectly
-
+            Toast.makeText(getApplicationContext(), "Data insertion unsuccessful", Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    public void viewData(View v) {
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0) {
+            showMessage("Error", "No data found in database");
+            //put a Log.d message and toast
+
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        //setup loop with moveToNext method
+            //append each COL to buffer
+            // use getString method
+
+        showMessage("Data", buffer.toString());
+    }
+
+    private void showMessage(String title, String message) {
+
     }
 
 
